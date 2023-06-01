9d2f076d7b544cecee84041f2151af360ab8b76c
daffasyaidina <91464359+daffasyaidina@users.noreply.github.com>
2023-06-02 01:15:37 +0700

    Update users-controllers.js

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,21 +1,23 @@
-const uuid = require('uuid/v4');
-const { validationResult } = require('express-validator');
+const uuid = require('uuid/v4'); 
+const { validationResult } = require('express-validator'); 
+const HttpError = require('../models/http-error'); 
 
-const HttpError = require('../models/http-error');
-
+// Dummy users array
 const DUMMY_USERS = [
   {
     id: 'u1',
-    name: 'Max Schwarz',
-    email: 'test@test.com',
-    password: 'testers'
+    name: 'John Doe',
+    email: 'johnD@test.com',
+    password: 'example'
   }
 ];
 
+// Handler for retrieving users
 const getUsers = (req, res, next) => {
   res.json({ users: DUMMY_USERS });
 };
 
+// Handler for user signup
 const signup = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -29,17 +31,18 @@ const signup = (req, res, next) => {
   }
 
   const createdUser = {
-    id: uuid(),
-    name, // name: name
+    id: uuid(), // Generate a unique user ID using the uuid() function
+    name, // Equivalent to name: name
     email,
     password
   };
 
   DUMMY_USERS.push(createdUser);
 
-  res.status(201).json({user: createdUser});
+  res.status(201).json({ user: createdUser }); // Return the created user in the response
 };
 
+// Handler for user login
 const login = (req, res, next) => {
   const { email, password } = req.body;
 
@@ -48,9 +51,10 @@ const login = (req, res, next) => {
     throw new HttpError('Could not identify user, credentials seem to be wrong.', 401);
   }
 
-  res.json({message: 'Logged in!'});
+  res.json({ message: 'Logged in!' }); // Return a success message in the response
 };
 
+// Export the defined functions to make them accessible in other files
 exports.getUsers = getUsers;
 exports.signup = signup;
 exports.login = login;
