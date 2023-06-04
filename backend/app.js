2b36727dce76b1df3f10ba2af976e78129ea586a
Sulthan Syuza Prabowo <sulthanprabowo7@gmail.com>
2023-06-04 23:29:34 +0700

    update

--- backend/app.js
+++ backend/app.js
@@ -60,7 +60,7 @@ mongoose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@custard.le00nl1.mongodb.net/${process.env.DB_NAME}`
   )
   .then(() => {
-    app.listen(5000);
+    app.listen(process.env.PORT || 5000);
   })
   .catch(err => {
     console.log(err);
