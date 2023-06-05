2b7c6af73ca6c3bf021f8b24083a82e83362c3d1
Sulthan Syuza Prabowo <sulthanprabowo7@gmail.com>
2023-06-05 14:41:07 +0700

    .

--- backend/app.js
+++ backend/app.js
@@ -18,16 +18,16 @@ app.use('/uploads/images', express.static(path.join('uploads', 'images')));
 app.use(express.static(path.join('public')));
 
 // Set headers for CORS
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-//   );
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
 
-//   next();
-// });
+  next();
+});
 
 // Routes
 app.use('/api/places', placesRoutes);
